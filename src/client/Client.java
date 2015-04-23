f004134c3e8839234cf5ba60f2ee65748b5b613d
Jeppe <jeppe.hartmund@gmail.com>
2015-04-23 15:37:02 +0200

    Implemented stuff!

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,6 +2,7 @@ package client;
 
 import com.scrom.model.Player;
 import com.scrom.model.SCROM;
+import com.scrom.model.action.ScromAction;
 
 /**
  * Created by jeppe on 12-03-15.
@@ -9,4 +10,7 @@ import com.scrom.model.SCROM;
 public class Client {
     Player player;
     SCROM game;
+    public void perform(ScromAction action){
+        assert action != null;
+    }
 }
--- src/client/ClientConnector.java
+++ src/client/ClientConnector.java
@@ -1,7 +1,62 @@
 package client;
 
+import com.scrom.model.action.ScromAction;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * Created by jeppe on 15-03-15.
  */
 public class ClientConnector {
+    final String SERVER_ADDRESS = "77.233.229.51";
+    final int SERVER_PORT = 60500;
+    Client client;
+    Boolean listening;
+    Socket channel;
+    ObjectInputStream ois;
+    ObjectOutputStream oos;
+    public ClientConnector(Client c){
+        client = c;
+        connect();
+        receive();
+        disconnect();
+    }
+
+    private void disconnect() {
+        try {
+            channel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void connect(){
+        try {
+            channel = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            ois = new ObjectInputStream(channel.getInputStream());
+            oos = new ObjectOutputStream(channel.getOutputStream());
+        } catch (Exception e){
+            System.out.println("OH NO!");
+        }
+    }
+    private void receive(){
+        listening = true;
+        while(listening){
+            ScromAction action = null;
+            try {
+                action = (ScromAction)ois.readObject();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            client.perform(action);
+        }
+    }
+    public void send(){
+
+    }
 }
