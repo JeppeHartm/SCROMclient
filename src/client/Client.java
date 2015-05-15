36291531e04441428d0987aaa468a88620f03422
Jeppe Hartmund <jeppe.hartmund@gmail.com>
2015-05-15 09:41:41 +0200

    herp derp

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,15 +2,30 @@ package client;
 
 import com.scrom.model.Player;
 import com.scrom.model.SCROM;
+import com.scrom.model.action.PlayerAction;
 import com.scrom.model.action.ScromAction;
+import com.scrom.model.action.ServerAction;
 
 /**
  * Created by jeppe on 12-03-15.
  */
 public class Client {
-    Player player;
     SCROM game;
+    //Perform is called on an instance of ScromAction to change the game state in accordance to it.
     public void perform(ScromAction action){
         assert action != null;
+        if(action instanceof PlayerAction){
+            PlayerAction pa = (PlayerAction)action;
+            switch(pa.getActionType()){
+                case CardPlayed:
+                    Player player = game.getPlayer(pa.getPlayerId());
+                    player.playCard(pa.getCard());
+            }
+        }else if(action instanceof ServerAction){
+            ServerAction sa = (ServerAction)action;
+            switch(sa.getActionType()){
+
+            }
+        }
     }
 }
